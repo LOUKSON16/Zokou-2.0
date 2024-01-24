015ced2fe7902c346dade26427bd891e0dcf34f1
LOUKSON16 <153290329+LOUKSON16@users.noreply.github.com>
2024-01-24 23:10:37 +0000

    fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -1,24 +1,24 @@
-const { zokou } = require("../framework/zokou");
+const { NARUTO } = require("../framework/NARUTO");
 const fancy = require("../commandes/style");
 
-zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk, commandeOptions) => {
+NARUTO({ nomCom: "fancy", categorie: "Fun", reaction: "🤘🏻" }, async (dest, zk, commandeOptions) => {
     const { arg, repondre, prefixe } = commandeOptions;
     const id = arg[0]?.match(/\d+/)?.join('');
     const text = arg.slice(1).join(" ");
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 zokou-md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Zokou-md', fancy));
+            return await repondre(`\nLoukson Exemple : ${prefixe}fancy 10 NARUTO-MD\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('Zokou-md', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
         if (selectedStyle) {
             return await repondre(fancy.apply(selectedStyle, text));
         } else {
-            return await repondre('_Style introuvable :(_');
+            return await repondre('_Yo Loukson 🤘🏻🌀Style introuvable _');
         }
     } catch (error) {
         console.error(error);
-        return await repondre('_Une erreur s\'est produite :(_');
+        return await repondre('_𝐋𝚯𝐔𝐊𝐒𝚯𝚴 🌀🤘🏻Une erreur s\'est produite :(_');
     }
 });
