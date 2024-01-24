9922ae20143fb0d430879a8ba8965dbbfb1171d6
LOUKSON16 <153290329+LOUKSON16@users.noreply.github.com>
2024-01-24 23:52:54 +0000

    test.js

--- commandes/test.js
+++ commandes/test.js
@@ -6,22 +6,22 @@ NARUTO({ nomCom: "test", reaction: "🤘🏻", nomFichier: __filename }, async (
     let z = 'Salut je m\'appelle *Zokou* \n\n ' + 'je suis un bot Whatsapp Multi-appareil ';
     let d = ' developpé par *Djalega++*';
     let varmess = z + d;
-    var img = 'https://wallpapercave.com/uwp/uwp3842939.jpeg';
+    var img = 'https://telegra.ph/file/251500a8a1b062796c273.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
 console.log("mon test");
 /*module.exports.commande = () => {
   var nomCom = ["test","t"]
-  var reaction="☺️"
+  var reaction="🤘🏻"
   return { nomCom, execute,reaction }
 };
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *Djalega++*'
+   let z ='Salut je m\'appelle *NARUTO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *🤜🏻𝐋𝚯𝐔𝐊𝐒𝚯𝚴-𝐔𝚭𝐔𝚳𝚫𝐊𝚰🤛🏻 *'
       let varmess=z+d
-      var img='https://wallpapercave.com/uwp/uwp3842939.jpeg'
+      var img='https://telegra.ph/file/251500a8a1b062796c273.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
